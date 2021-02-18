a49b449dc20ece311bdc8e45842dea206edc6107
Özge Yılmaz <79103460+ozgeyilmaaz@users.noreply.github.com>
2021-02-18 18:28:59 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,28 @@
-#Explain your work
+matrix = [[],
+          [],
+          []]
 
-#Question 1
-for x in range(a):
-	print(a)
+for x in range(9):
+    number = int(input("Please enter a prime number: "))
+    a = False
+    while a == False:
+        if number == 2:
+            break
+        for i in range(2, number):
+            if number % i == 0:
+                a = False
+                break
+            else:
+                a = True
+        if a == False:
+            number = int(input("Be sure to enter a prime number: "))
+
+    if len(matrix[0]) < 3:
+        matrix[0].append(number)
+    elif len(matrix[1]) < 3:
+        matrix[1].append(number)
+    else:
+        matrix[2].append(number)
+
+for b in range(3):
+    print(matrix[b])
