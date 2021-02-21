3002b5ae748793e89eb191bc7dd5feab19f5b5ab
Özge Yılmaz <79103460+ozgeyilmaaz@users.noreply.github.com>
2021-02-21 03:57:41 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,29 @@
-#Explain your work
+def prime_first(num):
+    print(num)
 
-#Question 1
-for x in range(a):
-	print(a)
+
+def prime_second(num):
+    print(num)
+
+
+for number in range(1000):
+    if number == 0:
+        print("Prime numbers from 0 to 500:")
+    elif number == 500:
+        print("Prime numbers from 500 to 1000:")
+
+    if number == 0 or number == 1 or number == 2:
+        continue
+    else:
+        control = True
+        for x in range(2, number):
+            if number % x == 0:
+                control = False
+                break
+            else:
+                control = True
+
+    if control and number < 500:
+        prime_first(number)
+    elif control and number >= 500:
+        prime_second(number)
